697484b9685ea52a018858c528746040e8d6427f
My Chiffon Nguyen <chiffonng136@gmail.com>
2025-11-16 19:06:22 -0800

    refactor(footer): change content

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,9 +1,7 @@
 function Footer() {
   return (
     <footer className="footer footer-center bg-base-300 text-base-content mt-auto p-4">
-      <aside>
-        <p>Copyright © 2025 - Codepath: Emanuel Pimentel + My "Chiffon" Nguyen</p>
-      </aside>
+      Copyright © 2025 SportConnect. Developed by Emanuel Pimentel and My Chiffon Nguyen with 🩵
     </footer>
   )
 }
